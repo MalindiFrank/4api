c38b6de011c689ba4e31f927f1de0709ca17e384
thmalinjhb024 <thmalinjhb024@student.wethinkcode.co.za>
2025-08-24 12:11:11 +0200

    added path routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import app from "../app.ts"
-import { getHtml, getJson } from "../controller/controller.ts";
+import { getHtml, getCollection, getCollectionItemById, getRandomCollectionItem, getNumberRandomItems } from "../controller/controller.ts";
 
 
 app.get("/", (c) => getHtml(c))
-
-app.get("/json", (c) => getJson(c))
+app.get("/:collection", (c) => getCollection(c))
+app.get("/:collection/:id", (c) => getCollectionItemById(c))
+app.get("/:collection/random", (c) => getRandomCollectionItem(c))
+app.get("/:collection/random/:n", (c) => getNumberRandomItems(c))
 
 
