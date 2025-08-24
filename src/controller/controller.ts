85bfabf218a57576b64d4f6bf8cca7744a5f828c
thmalinjhb024 <thmalinjhb024@student.wethinkcode.co.za>
2025-08-24 12:12:40 +0200

    created controller methods to handle the conresponding http request/response

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,10 +1,48 @@
 import { Context } from "@hono/hono";
 
-export function getHtml(c: Context): Response {
-  const html = "<h1>4 API</h1>"
-  return c.html(html)
+export async function getHtml(c: Context): Promise<Response> {
+  const html = await Deno.readTextFile("../../public/index.html");
+  return c.html(html);
 }
 
-export function getJson(c: Context): Response {
-  const json = {message: "json"}
-  return c.json(json)}
+export function getCollection(c: Context): Response {
+  const collectables: Array<cType> = getCollectionArray(c.req.param("collection"));
+
+  return c.json({
+    status: "OK",
+    length: collectables.length.toString(),
+    data: collectables,
+  });
+}
+
+export function getCollectionItemById(c: Context): Response {
+
+  const { collection, id } = c.req.param();
+  const collectables: object<cType> = getItemById(collection, Number.parseInt(id));
+
+  return c.json({
+    status: "OK",
+    data: collectables,
+  });
+}
+
+export function getRandomCollectionItem(c: Context): Response {
+  const collectables: object<cType> = getRandomItem(c.req.param("collection"));
+
+  return c.json({
+    status: "OK",
+    data: collectables,
+  });
+}
+
+export function getNumberRandomItems(c: Context): Response {
+
+  const { collection, n } = c.req.param();
+  const collectables: object<cType> = getRandomItems(collection, Number.parseInt(n));
+
+  return c.json({
+    status: "OK",
+    length: collectables.length.toString(),
+    data: collectables,
+  });
+}
